6fa0a80516cf40ff86070a967ea3db6c7a3ab714
khiemnguyen <39281424+nguyenkhiemv@users.noreply.github.com>
2019-08-22 20:06:41 -0700

    Update kin_mat_cal.R

--- script/kin_mat_cal.R
+++ script/kin_mat_cal.R
@@ -1,12 +1,20 @@
-#calculate kinship matrix for each gene based on cis SNP only ( 1 Mb window )
+######################calculate kinship matrix for each gene based on cis SNP only ( 1 Mb window )
+## save as kin_i: i is index (row # of gene pos matrix )
+## calculate for every 2000 genes #
+#############################################################################################
+
 #create set file by plink
-system( "sed -n '22001,24905 p' matrix.gene.pos.for.eqtl> gene.list.run" )
+system( "sed -n '2001,4000 p' matrix.gene.pos.for.eqtl> gene.list.run" )
 
+#extract cisSNP for each gene
 system( '~/softwares/plink/plink --noweb --bfile PsychEncode.QC.MAF0.05.EXPR.Nonrel
-        --make-set gene.list.run --make-set-border 1000 --write-set --out cisSNP_list_22001_24905')
+        --make-set gene.list.run --make-set-border 1000 --write-set --out cisSNP_list_2001_4900' )
 
-#create bedfile for each gene with cisSNP
-#system("~/softwares/plink/plink --noweb --bfile PsychEncode.QC.MAF0.05.EXPR.Nonrel --set cisSNP_list_2001_4000.set  --gene $(awk -v gene=$SGE_TASK_ID 'NR==gene' gene.symbol) --make-bed --out /scrapp/gene_$SGE_TASK_ID")
+#create bedfile for each gene 
+system( "~/softwares/plink/plink --noweb --bfile PsychEncode.QC.MAF0.05.EXPR.Nonrel 
+          --set cisSNP_list_2001_4000.set  --gene $(awk -v gene=$SGE_TASK_ID 'NR==gene' gene.symbol) 
+           --make-bed --out /scrapp/gene_$SGE_TASK_ID" )
 
 #calculate kinship matrix by GCTA
-#system('~/softwares/gcta/gcta64 --bfile /scrapp/gene_$SGE_TASK_ID  --autosome --make-grm --out ./cisSNP_kinship/kin_$SGE_TASK_ID')
+system( '~/softwares/gcta/gcta64 --bfile /scrapp/gene_$SGE_TASK_ID  
+        --autosome --make-grm --out ./cisSNP_kinship/kin_$SGE_TASK_ID')
