ea96ec0492cf9f005cc53ae8643ff586b3da9624
khiemnguyen <39281424+nguyenkhiemv@users.noreply.github.com>
2019-08-22 19:57:29 -0700

    Update kin_mat_cal.R

--- script/kin_mat_cal.R
+++ script/kin_mat_cal.R
@@ -1,11 +1,12 @@
 #calculate kinship matrix for each gene based on cis SNP only ( 1 Mb window )
-#SNP for all genes using 1Mb window
-system('~/softwares/plink/plink --bfile COMMONMIND.EUR.QC.EXPR.KEEP.rmAFF --make-set gene.list.1_1000 --make-set-border 1000 --write-set --out cisSNP_list')
+#create set file by plink
+system( "sed -n '22001,24905 p' matrix.gene.pos.for.eqtl> gene.list.run" )
 
+system( '~/softwares/plink/plink --noweb --bfile PsychEncode.QC.MAF0.05.EXPR.Nonrel
+        --make-set gene.list.run --make-set-border 1000 --write-set --out cisSNP_list_22001_24905')
 
-#extract cisSNP genotype bed file for each gene
-#system("~/softwares/plink/plink --noweb --bfile PsychEncode.QC.MAF0.05.EXPR.Nonrel --set cisSNP_list_22001_24905.set  --gene $(awk -v gene=$SGE_TASK_ID 'NR==gene' gene.symbol) --make-bed --out /scrapp/gene_$SGE_TASK_ID")
+#create bedfile for each gene with cisSNP
+#system("~/softwares/plink/plink --noweb --bfile PsychEncode.QC.MAF0.05.EXPR.Nonrel --set cisSNP_list_2001_4000.set  --gene $(awk -v gene=$SGE_TASK_ID 'NR==gene' gene.symbol) --make-bed --out /scrapp/gene_$SGE_TASK_ID")
 
-#use GCTA to calculate kinship matrix and store for GxEMM usex
+#calculate kinship matrix by GCTA
 #system('~/softwares/gcta/gcta64 --bfile /scrapp/gene_$SGE_TASK_ID  --autosome --make-grm --out ./cisSNP_kinship/kin_$SGE_TASK_ID')
-#
