63cdbebbe0c116a36d1f0994a421f08532f53688
khiemnguyen <39281424+nguyenkhiemv@users.noreply.github.com>
2019-08-27 17:55:47 -0700

    Update kin_mat_cal.R

--- script/kin_mat_cal.R
+++ script/kin_mat_cal.R
@@ -1,14 +1,7 @@
-######################calculate kinship matrix for each gene based on cis SNP only ( 1 Mb window )
+######################calculate kinship matrix for each gene based on cis SNP only ( 1 Mb window ) from plink set file
 ## save as kin_i: i is index (row # of gene pos matrix )
 ## calculate for every 2000 genes #
 #############################################################################################
-
-#create set file by plink
-system( "sed -n '2001,4000 p' matrix.gene.pos.for.eqtl> gene.list.run" )
-
-#extract cisSNP for each gene
-system( '~/softwares/plink/plink --noweb --bfile PsychEncode.QC.MAF0.05.EXPR.Nonrel
-        --make-set gene.list.run --make-set-border 1000 --write-set --out cisSNP_list_2001_4900' )
 
 #create bedfile for each gene 
 system( "~/softwares/plink/plink --noweb --bfile PsychEncode.QC.MAF0.05.EXPR.Nonrel 
